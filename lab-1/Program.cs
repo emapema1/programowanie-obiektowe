f0dac0efe3a16ecfb65620d924ac644c8d157063
Emilia.Kruzlak <Emilia.Kruzlak@PK3-19>
2022-03-09 18:27:52 +0100

    lab2

--- lab-1/Program.cs
+++ lab-1/Program.cs
@@ -7,13 +7,65 @@ namespace lab_1
         static void Main(string[] args)
         {
             Person person = Person.OfName("Adam");
-            Console.WriteLine(person.FirstName);
+            Console.WriteLine(person);
+            person.Equals(2);
             //
             Money money = Money.Of(12, Currency.PLN) ?? Money.Of(0, Currency.PLN);
             Console.WriteLine(money.Value + " " + money.Currency);
-            Money result = money * 0.22m;
+            Money result = 0.22m * money;
             Console.WriteLine(result.Value);
+            result = (money + Money.Of(5, money.Currency)) * 0.5m;
+            Console.WriteLine(result.Value);
+            if (money > result)
+            {
+                Console.WriteLine("money większe");
+            }
+            else
+            {
+                Console.WriteLine("result większe");
+            }
 
+
+            if (money == Money.Of(12, Currency.PLN))
+            {
+                Console.WriteLine("Równe");
+            }
+            else
+            {
+                Console.WriteLine("Różne");
+            }
+
+            int a = 10;
+            long b = 10L;
+            b = a;  //niejawne
+            a = (int)b;  //jawne
+            decimal price = money;
+            double cost = (double)money;
+            float c = (float)money;
+
+            Console.WriteLine("MONEY");
+            Console.WriteLine(money);
+            Console.WriteLine(money.ToString());
+            Console.WriteLine(a == b);
+            Console.WriteLine(money.Equals(Money.Of(13, Currency.PLN)));
+            IEquatable<Money> ie = money;
+
+
+            Money[] prices =
+            {
+                Money.Of(5,Currency.PLN),
+                Money.Of(3,Currency.USD),
+                Money.Of(15,Currency.PLN),
+                Money.Of(25,Currency.EUR),
+                Money.Of(54,Currency.PLN),
+                Money.Of(8,Currency.EUR),
+            };
+            Console.WriteLine("SORT");
+            Array.Sort(prices);
+            foreach(var p in prices)
+            {
+                Console.WriteLine();
+            }
         }
     }
 
@@ -56,6 +108,12 @@ namespace lab_1
                 }
             }
         }
+
+        public override string ToString()
+        {
+            return $"FirstName: {FirstName}";
+        }
+
     }
 
     public enum Currency
@@ -66,7 +124,7 @@ namespace lab_1
     }
 
 
-    public class Money
+    public class Money : IEquatable<Money>, IComparable<Money>
     {
         private readonly decimal _value;
         private readonly Currency _currency;
@@ -79,6 +137,48 @@ namespace lab_1
         public static Money operator *(Money money, decimal factor)
         {
             return Money.Of(money.Value * factor, money.Currency);
+        }
+
+        public static Money operator *(decimal factor, Money money)
+        {
+            return Money.Of(money.Value * factor, money.Currency);
+        }
+
+        public static bool operator >(Money a, Money b)
+        {
+            IsSameCurrencies(a, b);
+            return a.Value > b.Value;
+        }
+
+        public static bool operator <(Money a, Money b)
+        {
+            IsSameCurrencies(a, b);
+            return a.Value > b.Value;
+        }
+
+        public static Money operator +(Money a, Money b)
+        {
+            IsSameCurrencies(a, b);
+            return Money.Of(a.Value + b.Value, a.Currency);
+        }
+
+
+        public static implicit operator decimal(Money money)
+        {
+            return money.Value;
+        }
+        public static explicit operator double(Money money)
+        {
+            return (double)money.Value;
+        }
+
+
+        private static void IsSameCurrencies(Money a, Money b)
+        {
+            if (a.Currency != b.Currency)
+            {
+                throw new ArgumentException("Different currencies!");
+            }
         }
 
         public decimal Value
@@ -110,6 +210,43 @@ namespace lab_1
                 return new Money(value, currency);
             }
         }
-    }
 
+        ////////////////////////////////////////////////////////////////
+
+        public override string ToString()
+        {
+            return $"Value : {_value}, Currency: {_currency}";
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Money);
+        }
+
+
+        public bool Equals(Money other)
+        {
+            return other != null &&
+                   _value == other._value &&
+                   _currency == other._currency;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(_value, _currency);
+        }
+
+        public int CompareTo(Money other)
+        {
+            int curResult = _currency.CompareTo(other._currency);
+            if(curResult == 0)
+            {
+                return -_value.CompareTo(other._value);
+            }
+            else
+            {
+                return curResult;
+            }
+        }
+    }
 }
